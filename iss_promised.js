4b577d2679c4f4d94bac0234cb30db8f9a93aacd
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-15 18:43:16 -0700

    created next times func, required printPassTimes from index to index2

--- iss_promised.js
+++ iss_promised.js
@@ -14,5 +14,14 @@ const fetchISSFlyOverTimes = function(body) {
   return request(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
 };
 
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then((data) => {
+    const { response } = JSON.parse(data);
+    return response
+  });
+};
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+module.exports = { nextISSTimesForMyLocation };
