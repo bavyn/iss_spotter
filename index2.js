6438aa2e7ecb361dc54c3cb698a0908972ed2bb9
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-15 18:48:59 -0700

    added catch for errors

--- index2.js
+++ index2.js
@@ -4,4 +4,7 @@ const { printPassTimes } = require('./index');
 nextISSTimesForMyLocation()
   .then((passTimes) => {
     printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
   });
